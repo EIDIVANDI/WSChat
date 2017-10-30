79354285ae92a58c7989d407f25a7426717502ad
Eidivandi <Eidivandi@live.com>
2017-10-30 21:23:13 +0100

    Ajoutez des fichiers projet.

---
+++ WebSocketClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketHandlerExampleClient
+{
+    class Program
+    {
+        private static async Task SendToServer()
+        {
+            using (ClientWebSocket ws = new ClientWebSocket())
+            {
+                //Uri serverUri = new Uri("ws://websocketapiserver.com/WebSocketHandlerExample/WebSocketHandler.ashx");
+                Uri serverUri = new Uri("ws://websocketapiserver.com/api/websocket");
+               
+                    await ws.ConnectAsync(serverUri, CancellationToken.None);
+                    while (true)
+                    {
+                        Console.Write("Input message ('exit' to exit): ");
+                        string msg = Console.ReadLine();
+                        if (msg == "exit")
+                        {
+                            break;
+                        }
+
+                        ArraySegment<byte> bytesToSend = new ArraySegment<byte>(
+                            Encoding.UTF8.GetBytes(msg));
+
+                        await ws.SendAsync(bytesToSend, WebSocketMessageType.Text, true, CancellationToken.None);
+
+                        ArraySegment<byte> bytesReceived = new ArraySegment<byte>(new byte[1024]);
+
+                        WebSocketReceiveResult result = await ws.ReceiveAsync(
+                            bytesReceived, CancellationToken.None);
+                        Console.WriteLine(Encoding.UTF8.GetString(bytesReceived.Array, 0, result.Count));
+                        if (ws.State != WebSocketState.Open)
+                        {
+                            break;
+                        }
+                    }
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Task t = SendToServer();
+            t.Wait();
+        }
+    }
+}
---
+++ WebSocketHandler.ashx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web;
+using System.Web.WebSockets;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Text;
+
+namespace WebSocketHandlerExample
+{
+    public class WebSocketHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(WebSocketProcess);
+            }
+        }
+
+        public bool IsReusable { get { return false; } }
+
+        private async Task WebSocketProcess(AspNetWebSocketContext context)
+        {
+            while (true)
+            {
+                ArraySegment<byte> arraySegment = new ArraySegment<byte>(new byte[1024]);
+                // open the result. This is waiting asynchronously
+                WebSocketReceiveResult socketResult =
+                await context.WebSocket.ReceiveAsync(arraySegment,
+                CancellationToken.None);
+                // return the message to the client if the socket is still open
+                if (context.WebSocket.State == WebSocketState.Open)
+                {
+                    string message = Encoding.UTF8.GetString(arraySegment.Array, 0,
+                    socketResult.Count);
+                    message = "Your message: " + message + " at " +
+                    DateTime.Now.ToString();
+                    arraySegment = new
+                    ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+                    // Asynchronously send a message to the client
+                    await context.WebSocket.SendAsync(arraySegment,
+                    WebSocketMessageType.Text,
+                    true, CancellationToken.None);
+                }
+                else { break; }
+            }
+        }
+    }
+}
